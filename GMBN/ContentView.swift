8e6edf3413bbabdedfc08ad4f5582c8b87fd7ff1
Tancrede Chazallet <tancrede@bitbuildr.com>
2022-03-31 10:04:39 +0100

    Initial Commit

---
+++ GMBN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GMBN
+//
+//  Created by Tancrède Chazallet on 31/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GMBN/GMBNApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMBNApp.swift
+//  GMBN
+//
+//  Created by Tancrède Chazallet on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GMBNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
